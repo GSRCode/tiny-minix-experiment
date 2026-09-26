67f807ed28abe1ecddf703cb90c731227c8535df
root <root@MSI.localdomain>
2026-09-26 04:53:45 +0000

    what if B is created and then A and C notfies, test case

--- kernel/kernel_main.c
+++ kernel/kernel_main.c
@@ -12,8 +12,9 @@ void kernel_main(void)
     pit_init(100);
     proc_init();
     
-    proc_create(0, "proc_a", proc_a);
     proc_create(1, "proc_b", proc_b);
+    proc_create(0, "proc_a", proc_a);
+    
     proc_create(2, "proc_c", proc_c);
     
     
